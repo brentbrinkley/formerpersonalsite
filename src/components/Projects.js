7bf42dcd4d55c6f520c748e28897d4cca64d3433
Brent Brinkley <brentbrinkley@Danielles-iMac.local>
2019-11-23 04:52:36 -0500

    updated component to connect graphql properly

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,28 +2,37 @@ import React from "react"
 import { useStaticQuery } from "gatsby"
 
 const Projects = () => {
-  // const graphData = useStaticQuery(graphql`
-  //   query ProjectQuery {
-  //     allMarkdownRemark {
-  //       nodes {
-  //         frontmatter {
-  //           Project_Url
-  //           description
-  //           image
-  //           title
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
-  // const projects = graphData.allMarkdownRemark.nodes.frontmatter
+  const graphData = useStaticQuery(graphql`
+    query ProjectQuery {
+      allMarkdownRemark(
+        filter: { frontmatter: { templateKey: { eq: "project" } } }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              description
+              Project_Url
+            }
+          }
+        }
+      }
+    }
+  `)
 
-  // console.log(projects)
+  const data = graphData.allMarkdownRemark.edges
 
   return (
     <div>
-      <h3>Projects:</h3>
-      <p>Being Added Soon</p>
+      <h2>Projects:</h2>
+      <p>Coming Soon</p>
+      {/*}   {data.map(item => (
+        <div>
+          <h3>{item.node.frontmatter.title}</h3>
+          <p>{item.node.frontmatter.description}</p>
+          <a href={item.node.frontmatter.Project_Url}>New Kind's Site</a>
+        </div>
+      ))}{*/}
     </div>
   )
 }
